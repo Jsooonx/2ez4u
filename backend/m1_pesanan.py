78eae632d15dfaaaa181d51d0b9fd0e076359fd3
Gielang Fitrawan <jsooonx@gmail.com>
2026-09-17 08:41:43 +0700

    M1, step2: insert, delete

--- backend/m1_pesanan.py
+++ backend/m1_pesanan.py
@@ -85,4 +85,43 @@ class Array:
             raise IndexError(f"Indeks {i} di luar batas (ukuran array: {self.size})")
         return self.data[i]
 
+    def insert(self, i, v):
+        """menyimpan pesanan pada indeks ke-i
+        - VIP (i = 0): O(n)
+        - PRIORITAS (i = size // 2): O(n)
+        """
+        if i < 0 or i > self.size:
+            raise IndexError(f"Indeks penyisipan di luar batas: {i} (size:{self.size})")
+
+        #jika kapasitas penuh, gandakan kapasitas
+        if self.size == self.capacity:
+            self._resize(self.capacity * 2)
+
+        for k in range(self.size, i, -1):
+            self.data[k] = self.data[k - 1]
+
+        #letakkan elemen baru di slot indeks i
+        self.data[i] = v
+        self.size += 1
+
+    def delete(self, i):
+        """
+        Menghapus pesanan pada indeks ke-i dan menggeser elemen kanan ke kiri.
+        Kompleksitas: O(n).
+        """
+        if i < 0 or i >= self.size:
+            raise IndexError(f"Indeks di luar batas: {i} (size: {self.size})")
+
+        deleted_item = self.data[i]
+
+        #geser elemen di sebelah kanan indeks i satu langkah ke kiri
+        for k in range(i, self.size - 1):
+            self.data[k] = self.data[k + 1]
+
+        #kosongkan slot terakhir dan kurangi ukuran
+        self.data[self.size - 1] = None
+        self.size -= 1
+        return deleted_item
+
+
     
